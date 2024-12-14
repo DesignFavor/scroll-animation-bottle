f038a95e4f2035eb5a6269c6d5122506bc5da1ff
Designfavor <DesginFavor>
2024-12-13 21:48:43 -0800

    fix small bugs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,4 +30,4 @@ export default defineConfig({
       external: ['jquery'],
     },
   },
-})
+});
