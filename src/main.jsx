6d86d5b00eac3190929fe09ecdc8ef2d73231844
Designfavor <DesginFavor>
2024-12-13 22:35:07 -0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App';
 
-const container = document.getElementById('root');
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error('Root element not found');
-}
+createRoot(document.getElementById('root')).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,20 +14,20 @@ export default defineConfig({
   },
   build: {
     minify: true,
-    sourcemap: true, // Enable sourcemaps for debugging
     manifest: true,
-    outDir: 'build', // Output directory for the build files
+    outDir: 'build',
     rollupOptions: {
-      input: './index.html', // Ensure it points to index.html
+      input: './src/main.jsx',
       output: {
-        format: 'es', // Use ES module format
-        entryFileNames: 'main.js', // Name the entry file 'main.js'
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
         compact: true,
-        globals: {},
+        globals: {
+          jquery: '$',
+        },
       },
-      external: [
-        // This is not necessary in your case because 'main.js' is being output correctly, remove it
-      ],
+      external: ['jquery'],
     },
   },
-});
+})
